b08875834ba76a85dea46729c47e07fe95daf96b
kubil6y <lieqb2@gmail.com>
2022-09-03 22:23:12 +0300

    update

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 const blocked = ["twitter.com", "eksisozluk.com"];
 
 function main() {
-  const { origin } = new URL(window.location.href);
-  blocked.forEach((s) => {
-    if (origin.includes(s)) {
-      window.location.replace("https://www.youtube.com/watch?v=waAlgFq9Xq8");
-    }
-  });
+    const { origin } = new URL(window.location.href);
+    blocked.forEach((s) => {
+        if (origin.includes(s)) {
+            window.location.replace(
+                "https://www.youtube.com/watch?v=CK4QwwiQVBY"
+            );
+        }
+    });
 }
 main();
-
-// TODO
-// this works after we receive a response.
-// there must be a way to take action before.
-// cba
